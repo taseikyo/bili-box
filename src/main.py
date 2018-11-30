f80ff3bf5623d0406a3be199ac2b5b941da05bf6
LewisTian <lewis.smith.tian@gmail.com>
2018-11-30 11:02:53 +0800

    181130: add some features(see LOG.md)

--- src/main.py
+++ src/main.py
@@ -8,17 +8,26 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+from mwin import Ui_MWin
+from concurrent import futures
 import sys
 import os
+import shutil
 import time
 import datetime
-from mwin import Ui_MWin
-
+import requests
+import re
 
 MAX_WIDTH = 1920
 MAX_HEIGHT = 1080
 
+headers = {
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
+	'Cookie': 'CURRENT_QUALITY=64; DedeUserID=227050458; '
+}
+
 class MWin(QMainWindow, Ui_MWin):
+	'''嗶哩嗶哩盒子'''
 	def __init__(self, parent=None):
 		super(MWin, self).__init__(parent)
 		self.setupUi(self)
@@ -32,11 +41,34 @@ class MWin(QMainWindow, Ui_MWin):
 		# 变量
 		self.mFlag = False
 		self.mPosition = None
-		self.bgmPath = '' # 背景图片的路径
+		self.bgiPath = '' # 背景图片的路径
+		self.key = None # 视频链接 id
+		self.vRetrieval = VideoRetrieval()
 
 		self.setLogo()
-		self.setBackgroundImage()
+		self.setBackgroundImage(self.bgiPath)
 		self.connectSlots()
+
+	def resizeEvent(self, event):
+		'''窗口大小改变，背景图的大小也要改变
+		因为设置时已经定死了图片的尺寸'''
+		self.setBackgroundImage(self.bgiPath)
+
+	def mousePressEvent(self, event):
+		x = (event.globalPos() - self.pos()).x()
+		y = (event.globalPos() - self.pos()).y()
+		if event.button() == Qt.LeftButton:
+			self.mFlag = True
+			self.mPosition = event.globalPos() - self.pos()
+			event.accept()
+	
+	def mouseMoveEvent(self, QMouseEvent):
+		if Qt.LeftButton and self.mFlag:  
+			self.move(QMouseEvent.globalPos() - self.mPosition)
+			QMouseEvent.accept()
+			
+	def mouseReleaseEvent(self, QMouseEvent):
+		self.mFlag = False
 
 	def setLogo(self):
 		'''根据季节来更换logo(当前只有秋冬的;3)'''
@@ -52,33 +84,61 @@ class MWin(QMainWindow, Ui_MWin):
 			self.logo.setPixmap(QPixmap('images/bili-winter.png'))
 			self.logo.setToolTip('嗶哩嗶哩冬~')
 
-	def setBackgroundImage(self, path='images/background.jpg'):
+	def setBackgroundImage(self, path='images/background/background.jpg'):
 		'''设置主窗口的背景图片'''
-		# self.setStyleSheet(f'border-image: url({path});')
-		self.bgmPath = path
+		if self.bgiPath == '':
+			self.bgiPath = self.readSetting(0)
+			if not self.bgiPath:
+				return
+		elif self.bgiPath != path:
+			'''如果更新背景图片则将其复制到 background 目录并删除原来的图片'''
+			bgi = os.listdir('images/background')[0]
+			os.remove(f'images/background/{bgi}')
+			_, ext = os.path.splitext(path)
+			print(ext)
+			self.bgiPath = f'images/background/background{ext}'
+			shutil.copyfile(path, self.bgiPath)
+			self.modifySetting(self.bgiPath, 0)
+
 		self.setAutoFillBackground(True)
 		pal = self.palette()
-		# pal.setBrush(self.backgroundRole(), QPixmap(f'{path}'))
-		pixmap = QPixmap(f'{path}').scaled(self.size())
+		pixmap = QPixmap(f'{self.bgiPath}').scaled(self.size())
 		pal.setBrush(QPalette.Background, QBrush(pixmap))
 		self.setPalette(pal)
 
-	def resizeEvent(self, event):
-		'''窗口大小改变，背景图的大小也要改变
-		因为设置时已经定死了图片的尺寸'''
-		self.setBackgroundImage(self.bgmPath)
+	def readSetting(self, index=0):
+		'''读取第index行数据'''
+		try:
+			with open('settting.ini') as f:
+				return f.readlines()[index]
+		except Exception as e:
+			return None
+
+	def modifySetting(self, newdata, index=0):
+		'''修改第index行数据'''
+		try:
+			with open('settting.ini') as f:
+				lines = f.readlines()
+			lines[index] = newdata
+			with open('settting.ini', 'w') as f:
+				f.write(''.join(lines))
+		except Exception as e:
+			return None
 
 	def connectSlots(self):
 		'''关联所有信号槽'''
 		# 设置菜单下 Action
-		self.minSizeAction.triggered.connect(self.minSizeActionFunc)
-		self.bgmPathAction.triggered.connect(self.bgmPathActionFunc)
+		self.minSizeAction.triggered.connect(self.modifyMinSize)
+		self.bgiPathAction.triggered.connect(self.modifyBGIPath)
 
 		# 关于下的 Action
 		self.authorAction.triggered.connect(lambda: QDesktopServices.openUrl(QUrl('https://github.com/LewisTian')))
 		self.appAction.triggered.connect(lambda: QDesktopServices.openUrl(QUrl('https://github.com/LewisTian/bili-box')))
 
-	def minSizeActionFunc(self):
+		# 按钮
+		self.searchBtn.clicked.connect(self.resolveInput)
+
+	def modifyMinSize(self):
 		'''修改窗口的最小尺寸（默认是(960, 540)）'''
 		text, okPressed = QInputDialog.getText(self, '修改窗口尺寸', '输入尺寸:', QLineEdit.Normal, "输入两个数字 以空格分割")
 		if okPressed and text != '':
@@ -87,10 +147,12 @@ class MWin(QMainWindow, Ui_MWin):
 				x, y = int(raw[0]), int(raw[1])
 			except:
 				return
+
 			self.setMinimumSize(x, y)
-			print(f'{os.getcwd()}/mwin.py')
+			self.resize(x, y)
+			
 			try:
-				with open(f'{os.getcwd()}/mwin.py', encoding='utf-8') as f:
+				with open(f'/mwin.py', encoding='utf-8') as f:
 					lines = f.readlines()
 			except Exception as e:
 				return
@@ -105,42 +167,70 @@ class MWin(QMainWindow, Ui_MWin):
 			with open('mwin.py', 'w', encoding='utf-8') as f:
 				f.write(''.join(lines))
 
+	def modifyBGIPath(self):
+		'''修改背景图片(最好比例为16x9)'''
+		filename, ext = QFileDialog.getOpenFileName(self, '选择背景图片', 
+			QStandardPaths.standardLocations(QStandardPaths.PicturesLocation)[0], 
+			'Images (*.png *.jpg)')
+		if filename != '':
+			self.setBackgroundImage(filename)
 
-	def bgmPathActionFunc(self):
-		pass
+	def resolveInput(self):
+		index = self.comboBoxHome.currentIndex()
+		text = self.lineEditHome.text()
+		if text.strip(' ') == '':
+			return
+		if index == 0:
+			key = re.findall(r'av(\d+)', text)[0]
+			if not key: return
+			if self.key and self.key == key:
+				'''避免重复请求'''
+				return
+			self.key = key
+			self.request()
+			# self.vRetrieval.url = f'https://www.bilibili.com/video/av{self.key}'
+			# self.vRetrieval.start()
+		elif index == 1:
+			pass
+		else:
+			pass
 
-	def mousePressEvent(self, event):
-		x = (event.globalPos() - self.pos()).x()
-		y = (event.globalPos() - self.pos()).y()
-		if event.button() == Qt.LeftButton:
-			self.mFlag = True
-			self.mPosition = event.globalPos() - self.pos()
-			event.accept()
-			# self.setCursor(QCursor(Qt.OpenHandCursor))
+	def request(self, btype=0):
+		'''根据请求类型构造请求'''
+		if btype == 0:
+			url = f'https://www.bilibili.com/video/av{self.key}'
+			r = requests.get(url, headers=headers).text
+			title = re.findall(r'<h1 title="(.*?)">', r)[0].replace(' ','-')
+			cover = re.findall(r'<meta.*?itemprop="image" content="(.*?)"/><meta', r)
+			print(title, cover)
+			# zz = 'page":(.*?),"from":"vupload","part":"(.*?)","duration":'
+			# links = re.findall(zz, r.text)
+			# print(links)
+		elif btype == 1:
+			pass
+		else:
+			pass
 	
-	def mouseMoveEvent(self, QMouseEvent):
-		if Qt.LeftButton and self.mFlag:  
-			self.move(QMouseEvent.globalPos() - self.mPosition)
-			QMouseEvent.accept()
-			
-	def mouseReleaseEvent(self, QMouseEvent):
-		self.mFlag = False
-
 def mainSplash():
+	'''启动画面'''
 	app = QApplication(sys.argv)
-	splash = QSplashScreen(QPixmap('../images/background.jpg'))
-	splash.showMessage("启动界面", Qt.AlignLeft | Qt.AlignBottom, Qt.black)
+	pixmap = QPixmap('images/background.jpg').scaled(720, 405)
+	splash = QSplashScreen(pixmap)
+	splash.showMessage('正在加载资源中...', Qt.AlignLeft | Qt.AlignBottom, Qt.white)
 	splash.show()
-	time.sleep(2)
 	app.processEvents()
-	window = QWidget()
-	window.show()
-	splash.finish(window)
+	time.sleep(2)
+	w = MWin()
+	w.show()
+	splash.finish(w)
 	del splash
 	app.exec()
 
-if __name__ == '__main__':
+def main():
 	app = QApplication(sys.argv)
 	w = MWin()
 	w.show()
 	sys.exit(app.exec_())
+
+if __name__ == '__main__':
+	main()
